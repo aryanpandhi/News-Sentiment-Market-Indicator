ef314c6babd9828a584b442324cce381552bf533
ap <aryan.pandhi@gmail.com>
2020-07-05 22:58:28 +0530

    read data

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,5 +5,5 @@ market_df = pd.read_csv('SPY_market_data.csv')
 dates = market_df['Date']
 difference = ((market_df['Close'] - market_df['Open'])/market_df['Open'])*100
 market_movement = pd.DataFrame({'Date': dates, 'Difference': difference })
-print(market_movement.head())
-print(market_movement.describe())
+# print(market_movement.head())
+# print(market_movement.describe())
